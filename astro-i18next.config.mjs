5e831ba0a8ad921d1828b5a7634e61d70b557118
Facundo Garcia <zogar1993@gmail.com>
2022-12-19 08:57:45 -0300

    reproduce error

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLocale: "en-US",
+  locales: ["en-US", "es"],
+
+  i18nextServer: {
+    backend: {
+      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
+    },
+  },
+}
